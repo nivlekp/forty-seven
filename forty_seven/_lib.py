71bbd93dae9d36b5e481ce0ca3c828d5b539f38c
nivlekp <osamupang@gmail.com>
2022-01-29 21:28:13 +1100

    change abjad.OrderedDict to dict

--- forty_seven/_lib.py
+++ forty_seven/_lib.py
@@ -6,134 +6,94 @@ import abjad
 import pang
 from abjadext import nauert
 
-instruments = abjad.OrderedDict([("Flute", abjad.Flute())])
+instruments = {"Flute": abjad.Flute()}
 
 start_pitch = instruments["Flute"].pitch_range.start_pitch
 stop_pitch = instruments["Flute"].pitch_range.stop_pitch
 
-metronome_marks = abjad.OrderedDict(
-    [
-        ("60", abjad.MetronomeMark((1, 4), 60)),
-        ("72", abjad.MetronomeMark((1, 4), 72)),
-    ]
-)
+metronome_marks = {
+    "60": abjad.MetronomeMark((1, 4), 60),
+    "72": abjad.MetronomeMark((1, 4), 72),
+}
 
-sieves = abjad.OrderedDict(
-    [
-        ("A", abjad.Pattern(indices=[0, 3], period=10)),
-        ("B", abjad.Pattern(indices=[0, 2, 4, 5], period=7)),
-        ("C", abjad.Pattern(indices=[0, 1, 2, 4, 8, 10], period=11)),
-    ]
-)
+sieves = {
+    "A": abjad.Pattern(indices=[0, 3], period=10),
+    "B": abjad.Pattern(indices=[0, 2, 4, 5], period=7),
+    "C": abjad.Pattern(indices=[0, 1, 2, 4, 8, 10], period=11),
+}
 
-registrations = abjad.OrderedDict(
-    [
-        ("full", (start_pitch, stop_pitch)),
-        ("low", (start_pitch, stop_pitch + abjad.NamedInterval(-24))),
-        (
-            "middle",
-            (
-                start_pitch + abjad.NamedInterval(12),
-                stop_pitch + abjad.NamedInterval(-12),
-            ),
-        ),
-        (
-            "mid-high",
-            (
-                start_pitch + abjad.NamedInterval(12),
-                stop_pitch,
-            ),
-        ),
-        ("high", (start_pitch + abjad.NamedInterval(24), stop_pitch)),
-    ]
-)
+registrations = {
+    "full": (start_pitch, stop_pitch),
+    "low": (start_pitch, stop_pitch + abjad.NamedInterval(-24)),
+    "middle": (
+        start_pitch + abjad.NamedInterval(12),
+        stop_pitch + abjad.NamedInterval(-12),
+    ),
+    "mid-high": (
+        start_pitch + abjad.NamedInterval(12),
+        stop_pitch,
+    ),
+    "high": (start_pitch + abjad.NamedInterval(24), stop_pitch),
+}
 
-search_trees = abjad.OrderedDict(
-    [
-        (
-            "A",
-            nauert.UnweightedSearchTree(
-                definition={
-                    2: {2: None, 3: None},
-                    3: {2: None, 3: None},
-                    5: {2: None},
-                    7: None,
-                }
-            ),
-        ),
-        (
-            "B",
-            nauert.UnweightedSearchTree(
-                definition={
-                    2: {2: {2: None}, 3: None, 5: None, 7: None},
-                    3: {2: None, 3: None, 5: None},
-                    5: {2: None, 3: None},
-                    7: {2: None},
-                },
-            ),
-        ),
-        (
-            "C",
-            nauert.WeightedSearchTree(
-                definition={
-                    "divisors": (2, 3, 5, 7),
-                    "max_depth": 2,
-                    "max_divisions": 2,
-                },
-            ),
-        ),
-        (
-            "D",
-            nauert.WeightedSearchTree(
-                definition={
-                    "divisors": (2, 3, 5, 7, 11),
-                    "max_depth": 1,
-                    "max_divisions": 4,
-                }
-            ),
-        ),
-    ]
-)
+search_trees = {
+    "A": nauert.UnweightedSearchTree(
+        definition={
+            2: {2: None, 3: None},
+            3: {2: None, 3: None},
+            5: {2: None},
+            7: None,
+        }
+    ),
+    "B": nauert.UnweightedSearchTree(
+        definition={
+            2: {2: {2: None}, 3: None, 5: None, 7: None},
+            3: {2: None, 3: None, 5: None},
+            5: {2: None, 3: None},
+            7: {2: None},
+        },
+    ),
+    "C": nauert.WeightedSearchTree(
+        definition={
+            "divisors": (2, 3, 5, 7),
+            "max_depth": 2,
+            "max_divisions": 2,
+        },
+    ),
+    "D": nauert.WeightedSearchTree(
+        definition={
+            "divisors": (2, 3, 5, 7, 11),
+            "max_depth": 1,
+            "max_divisions": 4,
+        }
+    ),
+}
 
-q_schemas = abjad.OrderedDict(
-    [
-        (
-            "A",
-            nauert.MeasurewiseQSchema(
-                search_tree=search_trees["A"],
-                tempo=metronome_marks["72"],
-                time_signature=(7, 8),
-            ),
-        ),
-        (
-            "B",
-            nauert.MeasurewiseQSchema(
-                search_tree=search_trees["B"],
-                tempo=metronome_marks["72"],
-                time_signature=(7, 8),
-                use_full_measure=True,
-            ),
-        ),
-        (
-            "C",
-            nauert.MeasurewiseQSchema(
-                search_tree=search_trees["C"],
-                tempo=metronome_marks["72"],
-                time_signature=(7, 8),
-                use_full_measure=True,
-            ),
-        ),
-        (
-            "D",
-            nauert.MeasurewiseQSchema(
-                search_tree=search_trees["D"],
-                tempo=metronome_marks["72"],
-                time_signature=(7, 8),
-                use_full_measure=True,
-            ),
-        ),
-    ]
-)
+q_schemas = {
+    "A": nauert.MeasurewiseQSchema(
+        search_tree=search_trees["A"],
+        tempo=metronome_marks["72"],
+        time_signature=(7, 8),
+    ),
+    "B": nauert.MeasurewiseQSchema(
+        search_tree=search_trees["B"],
+        tempo=metronome_marks["72"],
+        time_signature=(7, 8),
+        use_full_measure=True,
+    ),
+    "C": nauert.MeasurewiseQSchema(
+        search_tree=search_trees["C"],
+        tempo=metronome_marks["72"],
+        time_signature=(7, 8),
+        use_full_measure=True,
+    ),
+    "D": nauert.MeasurewiseQSchema(
+        search_tree=search_trees["D"],
+        tempo=metronome_marks["72"],
+        time_signature=(7, 8),
+        use_full_measure=True,
+    ),
+}
 
 overtone_series_intervals = [
     0,
