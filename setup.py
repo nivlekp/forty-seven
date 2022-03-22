7a662ffe81334fd2acdd7ce7b4a31e0d46c70bab
nivlekp <osamupang@gmail.com>
2022-03-22 22:08:20 +1100

    Use setuptools rather than distutils for setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+import setuptools
 
 if __name__ == "__main__":
-    setup(
+    setuptools.setup(
         author="Tsz Kiu Pang",
         author_email="osamupang@gmail.com",
         install_requires=["abjad", "abjad-ext-nauert"],
-        name="forty-seven",
-        packages=["forty-seven"],
+        name="forty_seven",
+        packages=["forty_seven"],
         version="0.1",
         zip_safe=False,
     )
