a399ace4fc15121711c4b00bc73fdfb12dc136a6
nivlekp <osamupang@gmail.com>
2022-07-07 21:57:27 +1000

    update metadata

--- forty_seven/segments/b/__metadata__.py
+++ forty_seven/segments/b/__metadata__.py
@@ -1,17 +1,15 @@
 import abjad
 
-metadata = dict(
-    {
-        "empty_beatspan": abjad.Duration(0, 1),
-        "last_tempo": MetronomeMark(
-            reference_duration=Duration(1, 4),
-            units_per_minute=72,
-            textual_indication=None,
-            custom_markup=None,
-            decimal=None,
-            hide=False,
-        ),
-        "last_time_signature": TimeSignature(pair=(7, 8), hide=False, partial=None),
-        "segment_name": "b",
-    }
-)
+metadata = {
+    "last_tempo": MetronomeMark(
+        reference_duration=Duration(1, 4),
+        units_per_minute=72,
+        textual_indication=None,
+        custom_markup=None,
+        decimal=False,
+        hide=False,
+    ),
+    "last_time_signature": TimeSignature(pair=(7, 8), hide=False, partial=None),
+    "empty_beatspan": Duration(0, 1),
+    "segment_name": "b",
+}
